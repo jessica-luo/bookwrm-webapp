c89abc506ea418823b6dea9d04ca1e4efa20c38c
cmstrandwold <strandwold.c@northeastern.edu>
2021-12-06 15:05:47 -0500

    set up basic route structure

--- web-dev-fp/src/App.js
+++ web-dev-fp/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+import HomeScreen from "./components/HomeScreen";
+import DetailsScreen from "./components/DetailsScreen";
+import LoginScreen from "./components/LoginScreen";
+import PrivacyPolicyScreen from "./components/PrivacyPolicyScreen";
+import ProfileScreen from "./components/ProfileScreen";
+import SearchScreen from "./components/SearchScreen";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+                <Route path={["/", "/home"]} exact={true} component={HomeScreen}/>
+                <Route path={["/search"]} exact={true} component={SearchScreen}/>
+                <Route path={["/details"]} exact={true} component={DetailsScreen}/>
+                <Route path={["/profile"]} exact={true} component={ProfileScreen}/>
+                <Route path={["/login"]} exact={true} component={LoginScreen}/>
+                <Route path={["/privacy"]} exact={true} component={PrivacyPolicyScreen}/>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ web-dev-fp/src/components/PrivacyPolicyScreen/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrivacyPolicyScreen = () => {
+    return (
+        <>
+            <h1>Privacy Policy Screen</h1>
+        </>
+    )
+};
+
+export default PrivacyPolicyScreen;
