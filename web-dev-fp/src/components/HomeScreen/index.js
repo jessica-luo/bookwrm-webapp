c1b5249932eaaa030509d22190ddcde91295035f
cmstrandwold <strandwold.c@husky.neu.edu>
2021-12-09 20:23:12 -0500

    Update index.js

--- web-dev-fp/src/components/HomeScreen/index.js
+++ web-dev-fp/src/components/HomeScreen/index.js
@@ -1,29 +1,11 @@
 import React, {useEffect, useState} from "react";
 import NavigationComponent from "../NavigationComponent";
-import bookService from "../../services/bookService";
 
 const HomeScreen = () => {
-    const [books, setBooks] = useState([]);
-    useEffect(() => {
-        return bookService.findAllBooks()
-            .then(books => setBooks(books));
-    }, []);
     return (
         <>
             <div>
                 <NavigationComponent activeLink={'/home'}/>
-                <div className={"container main-container"}>
-                    <ul className="list-group">
-                        {
-                            books.map(book =>
-                                <li key={book._id}
-                                    className="list-group-item">
-                                    {book.title}
-                                </li>
-                            )
-                        }
-                    </ul>
-                </div>
             </div>
 
 
