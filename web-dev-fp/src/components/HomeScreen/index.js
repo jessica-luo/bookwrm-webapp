89ca545ef9b807638b8d84691f3b3f4740c168bc
cmstrandwold <strandwold.c@northeastern.edu>
2021-12-09 20:18:52 -0500

    fixed homepage

--- web-dev-fp/src/components/HomeScreen/index.js
+++ web-dev-fp/src/components/HomeScreen/index.js
@@ -1,32 +1,9 @@
-import React, {useEffect, useState} from "react";
 import NavigationComponent from "../NavigationComponent";
-import bookService from "../../services/bookService";
 
 const HomeScreen = () => {
-    const [books, setBooks] = useState([]);
-    useEffect(() => {
-        return bookService.findAllBooks()
-            .then(books => setBooks(books));
-    }, []);
     return (
         <>
-            <div>
-                <NavigationComponent activeLink={'/home'}/>
-                <div className={"container main-container"}>
-                    <ul className="list-group">
-                        {
-                            books.map(book =>
-                                <li key={book._id}
-                                    className="list-group-item">
-                                    {book.title}
-                                </li>
-                            )
-                        }
-                    </ul>
-                </div>
-            </div>
-
-
+            <NavigationComponent activeLink={'/home'}/>
         </>
     )
 };
