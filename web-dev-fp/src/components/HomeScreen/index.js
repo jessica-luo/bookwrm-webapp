04c20aabc72ffbeb5f2fe03a7967c1dbf7b089d1
jessica-luo <jessicaluoaustin@gmail.com>
2021-12-09 20:05:15 -0500

    home page changes

--- web-dev-fp/src/components/HomeScreen/index.js
+++ web-dev-fp/src/components/HomeScreen/index.js
@@ -1,12 +1,53 @@
 import React from "react";
 import NavigationComponent from "../NavigationComponent";
+import BookListItem from "./BookListItem";
+import featuredbooks from "./featuredbooks";
+import trendingbooks from "./trendingbooks";
 
 const HomeScreen = () => {
     return (
         <>
             <NavigationComponent activeLink={'/home'}/>
-            <div className={"container main-container bg-none"}>
+            <h1>Home Screen</h1>
+            <h1 className="text-center mt-5 text-success font-weight-bold">BookWrm <i className={"fas fa-book"}/></h1>
+            <p className="text-center">/ˈbo͝okˌwərm/</p>
+            <p className="text-center"> 1. a person unusually devoted to reading and study</p>
+            <div className={"container main-container bg-none mt-5"}>
+                <div className="container">
+                    <div className="row p-4 border rounded">
+                        <div className="col text-success"> <h2>Featured</h2></div>
+                        <div className="col text-success"> <h2>Trending</h2></div>
+                        <div className="w-100"></div>
+                        <div className="col text-secondary">
+                            <ul className="list-group">
+                                {
+                                    featuredbooks.map(book =>
+                                        <BookListItem book={book}/>
+                                    )
+                                }
+                            </ul>
+                        </div>
+                        <div className="col text-secondary">
+                            <ul className="list-group">
+                                {
+                                    trendingbooks.map(book =>
+                                        <BookListItem book={book}/>
+                                    )
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
 
+                <div className="container mt-5 mb-5">
+                    <div className="row p-4 border rounded">
+                        <div className="col text-success"> <h2>Your To-Read List</h2></div>
+                        <div className="col text-success"> <h2>Revisit: Books You've Recently Finished</h2></div>
+                        <div className="w-100"></div>
+                        <div className="col text-secondary">Books (when logged in?)</div>
+                        <div className="col text-secondary">Books (when logged in?)</div>
+                    </div>
+                </div>
             </div>
 
         </>
--- web-dev-fp/src/components/LoginScreen/index.js
+++ web-dev-fp/src/components/LoginScreen/index.js
@@ -1,13 +1,75 @@
 import React from "react";
 import NavigationComponent from "../NavigationComponent";
+import {createUser} from "../../services/userService";
+import {createAuthor} from "../../services/authorService";
 
-const LoginScreen = () => {
-    return (
-        <>
-            <NavigationComponent activeLink={'/login'}/>
-            <h1>Login Screen</h1>
-        </>
-    )
+export default class LoginScreen extends React.Component {
+    state = {
+        username: '',
+        password: '',
+        email: '',
+        firstName: '',
+        lastName: ''
+    }
+
+    registerUser = (user) =>
+        createUser(user)
+            .then(user => this.props.history.push('/profile'))
+
+    registerAuthor = (user) =>
+        createAuthor(user)
+            .then(user => this.props.history.push('profile'))
+
+    render() {
+        return (
+            <>
+                <NavigationComponent activeLink={'/login'}/>
+                <h1>Login Page</h1>
+
+                <div className={"container main-container bg-none"}>
+                    <h1 className={"mt-5"}>Login</h1>
+                    <input placeholder={"username"} className={`form-control mt-1`}/>
+                    <input placeholder={"password"} type={"password"} className={`form-control mt-1`}/>
+                    <button className={`btn btn-success mt-1`}>Login
+                    </button>
+
+                    <h1 className={"mt-5"}>Register</h1>
+                    <input value={this.state.username}
+                           onChange={(e) => this.setState({
+                               username: e.target.value
+                           })}
+                           placeholder={"username"} className={`form-control mt-1`}/>
+                    <input value={this.state.password}
+                           onChange={(e) => this.setState({
+                               password: e.target.value
+                           })}
+                           placeholder={"password"} type={"password"} className={`form-control mt-1`}/>
+                    <input value={this.state.email}
+                           onChange={(e) => this.setState({
+                               email: e.target.value
+                           })}
+                           placeholder={"email"} className={`form-control mt-1`}/>
+                    <input value={this.state.firstName}
+                           onChange={(e) => this.setState({
+                               firstName: e.target.value
+                           })}
+                           placeholder={"first name"} className={`form-control mt-1`}/>
+                    <input value={this.state.lastName}
+                           onChange={(e) => this.setState({
+                               lastName: e.target.value
+                           })}
+                           placeholder={"last name"} className={`form-control mt-1`}/>
+                    <button onClick={() => this.registerUser(this.state)}
+                            className={`btn btn-success`}>Register as a Reader
+                    </button>
+
+                    <button onClick={() => this.registerAuthor(this.state)}
+                            className={`btn btn-primary`}>Register as an Author
+                    </button>
+
+                    <p className={"mb-5"}>*Author accounts have different permissions from readers.</p>
+                </div>
+            </>
+        )
+    }
 };
-
-export default LoginScreen;
