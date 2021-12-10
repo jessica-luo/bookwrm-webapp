e173f1ca33076c0d53b33594d40ee3310fd1a4c1
cmstrandwold <strandwold.c@northeastern.edu>
2021-12-09 20:21:48 -0500

    fixed homepage

--- web-dev-fp/src/components/LoginScreen/index.js
+++ web-dev-fp/src/components/LoginScreen/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavigationComponent from "../NavigationComponent";
+import Footer from "../FooterComponent";
 
 const LoginScreen = () => {
     return (
         <>
             <NavigationComponent activeLink={'/login'}/>
             <h1>Login Screen</h1>
+            <Footer/>
         </>
     )
 };
