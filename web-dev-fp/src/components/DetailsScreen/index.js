9419f64f650c5ac48c8a5d197abe3ae5055dfd7c
Sheryl <deakin.s@husky.neu.edu>
2021-12-10 19:53:30 -0500

    search done, working on details

--- web-dev-fp/src/components/DetailsScreen/index.js
+++ web-dev-fp/src/components/DetailsScreen/index.js
@@ -1,12 +1,66 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NavigationComponent from "../NavigationComponent";
+import {Link, useParams} from "react-router-dom";
 
 const DetailsScreen = () => {
+    const params = useParams();
+    // let navigate = useNavigate();
+    const isbn = params.searchTerm || '9780980200447';
+    // const [searchTerm, setSearchTerm] = useState(isbn);
+    const [book, setBook] = useState([]);
 
+
+    const getBookDetails = () =>
+    {
+        let fetchRes = fetch(
+            `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=details&format=json`);
+
+        fetchRes.then(res =>
+            res.json()).then(d => {
+                // console.log(" hi", d)
+            setBook(d.Search)
+        }).then(results => console.log("hi", book))
+    }
+
+    console.log("book", book)
+    useEffect(getBookDetails, []);
     return (
         <>
             <NavigationComponent activeLink={'/details'}/>
-            <h1>Details Screen</h1>
+            <br/><br/><br/>
+            <div className="container">
+                <h1>Details Screen</h1>
+                <></>
+                {/*<li key={book.details._id}*/}
+                {/*    className="list-group-item">*/}
+                {/*    <button*/}
+                {/*        className="btn btn-success float-end ms-2">*/}
+                {/*        Add*/}
+                {/*    </button>*/}
+                {/*    <table>*/}
+                {/*        <tbody>*/}
+                {/*        <tr>*/}
+                {/*            <td className="align-text-top">*/}
+                {/*                <h4>{book.details.title}</h4>*/}
+                {/*            </td>*/}
+                {/*            <td>*/}
+                {/*                Image*/}
+                {/*            </td>*/}
+                {/*            <td>*/}
+
+                {/*            </td>*/}
+                {/*        </tr>*/}
+                {/*        <tr>*/}
+                {/*            <td>Author: {book.details.author_name}</td>*/}
+                {/*            <td>Date Published: {book.details.first_publish_year}</td>*/}
+                {/*            <td>ISBN: </td>*/}
+                {/*        </tr>*/}
+                {/*        </tbody>*/}
+                {/*    </table>*/}
+                {/*</li>*/}
+            </div>
+
+
         </>
     )
 };
--- web-dev-fp/src/components/SearchScreen/index.js
+++ web-dev-fp/src/components/SearchScreen/index.js
@@ -1,11 +1,147 @@
-import React from "react";
+import {useState, useEffect} from "react";
+
+import {Link, useParams } from "react-router-dom";
+
 import NavigationComponent from "../NavigationComponent";
+import DetailsScreen from "../DetailsScreen";
 
 const SearchScreen = () => {
+    const params = useParams();
+    // let navigate = useNavigate();
+    const bookTitle = params.searchTerm || 'batman';
+    const [searchTerm, setSearchTerm] = useState(bookTitle);
+    const [books, setBooks] = useState([]);
+
+    const findBooks = () =>
+    {
+
+        let fetchRes = fetch(
+            `https://openlibrary.org/search.json?title=${searchTerm}`);
+
+        fetchRes.then(res =>
+            res.json()).then(d => {
+            setBooks(d.docs)
+        }).then(results => console.log(books)).catch(e => console.log("error", e))
+    }
+
+    useEffect(findBooks, []);
     return (
         <>
             <NavigationComponent activeLink={'/search'}/>
-            <h1>Search Screen</h1>
+            <br/><br/><br/>
+            <div className="container">
+                <h1>Search Screen</h1>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        <div>
+                            <button
+                                type="button"
+                                onClick={findBooks}
+                                className="btn btn-primary float-end">
+                                Search
+                            </button>
+                            <input
+                                onChange={(e) => setSearchTerm(e.target.value)}
+                                value={searchTerm}
+                                placeholder="Search for books here!"
+                                className="form-control"
+                                style={{width: "80%"}}/>
+                            <br/>
+                        </div>
+                    </li>
+                    <li className="list-group-item">
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td className="align-text-top">
+                                    <h1>Book Results</h1>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                    <li className="list-group-item">
+                        <button
+                            className="btn btn-success float-end ms-2">
+                            Add
+                        </button>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td className="align-text-top">
+                                    <h4>Title of Book</h4>
+                                </td>
+                                <td>
+                                    Image
+                                </td>
+                                <td>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Author</td>
+                                <td>Date Published</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                        {
+                            books && books.map(book =>
+                                <li key={book._id}
+                                    className="list-group-item">
+                                    <button
+                                        className="btn btn-success float-end ms-2">
+                                        Add
+                                    </button>
+                                    <Link to={"/details"}>
+                                        <button
+                                            onClick={() => DetailsScreen(book)}
+                                            className="btn btn-warning float-end ms-2">
+                                            Details
+                                        </button>
+                                    </Link>
+                                    <table>
+                                        <tbody>
+                                        <tr>
+                                            <td className="align-text-top">
+                                                <h4>{book.title}</h4>
+                                            </td>
+                                            <td>
+                                                Image
+                                            </td>
+                                            <td>
+
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>Author: {book.author_name}</td>
+                                            <td>Date Published: {book.first_publish_year}</td>
+                                            <td>ISBN: </td>
+                                                {
+                                                    book.isbn && book.isbn.map(bookisbn =>
+                                                    <tr>
+                                                        <td>
+                                                            {bookisbn.toString()}
+                                                        </td>
+                                                        <td>
+                                                            <Link to={`/details/${bookisbn}`}>
+                                                                <button
+                                                                    onClick={() => DetailsScreen(book)}
+                                                                    className="btn btn-warning float-end ms-2">
+                                                                    Details
+                                                                </button>
+                                                            </Link>
+                                                        </td>
+                                                    </tr>
+                                            )}
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </li>
+                            )
+                        }
+                </ul>
+            </div>
         </>
     )
 };
