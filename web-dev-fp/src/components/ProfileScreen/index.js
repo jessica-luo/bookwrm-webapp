f28f5b9c3e833170b392e379e96fa1d00e03115d
jessica-luo <jessicaluoaustin@gmail.com>
2021-12-11 14:02:24 -0500

    profile screen remove react.Component

--- web-dev-fp/src/components/ProfileScreen/index.js
+++ web-dev-fp/src/components/ProfileScreen/index.js
@@ -1,23 +1,22 @@
 import React from "react";
 import NavigationComponent from "../NavigationComponent";
 import Footer from "../FooterComponent";
+import {useState} from "react";
 
-export default class ProfileScreen extends React.Component {
+const ProfileScreen = () => {
 
-    state = {
-        profile: {
-            username: '',
-            password: '',
-            email: '',
-            firstName: '',
-            lastName: ''
-        }
-    }
+    const [user, setUser] = useState({
+        username: '',
+        password: '',
+        email: '',
+        firstName: '',
+        lastName: ''
+    })
 
-    edit = (user) =>
+    const edit = (user) =>
         console.log(user)
 
-    render() {
+    {
         return (
             <>
                 <NavigationComponent activeLink={'/login'}/>
@@ -25,32 +24,57 @@ export default class ProfileScreen extends React.Component {
                 <div className={"container main-container bg-none"}>
 
                     <h1 className={"mt-5"}>Update Profile</h1>
-                    <input value={this.state.username}
-                           onChange={(e) => this.setState({
-                               username: e.target.value
+                    <input value={user.username}
+                           onChange={(e) => setUser({
+                               ...user,
+                               username: e.target.value,
+                               password: user.password,
+                               email: user.email,
+                               firstName: user.firstName,
+                               lastName: user.lastName
                            })}
                            placeholder={"username"} className={`form-control mt-1`}/>
-                    <input value={this.state.password}
-                           onChange={(e) => this.setState({
-                               password: e.target.value
+                    <input value={user.password}
+                           onChange={(e) => setUser({
+                               ...user,
+                               username: user.username,
+                               password: e.target.value,
+                               email: user.email,
+                               firstName: user.firstName,
+                               lastName: user.lastName
                            })}
                            placeholder={"password"} type={"password"} className={`form-control mt-1`}/>
-                    <input value={this.state.email}
-                           onChange={(e) => this.setState({
-                               email: e.target.value
+                    <input value={user.email}
+                           onChange={(e) => setUser({
+                               ...user,
+                               username: user.username,
+                               password: user.password,
+                               email: e.target.value,
+                               firstName: user.firstName,
+                               lastName: user.lastName
                            })}
                            placeholder={"email"} className={`form-control mt-1`}/>
-                    <input value={this.state.firstName}
-                           onChange={(e) => this.setState({
-                               firstName: e.target.value
+                    <input value={user.firstName}
+                           onChange={(e) => setUser({
+                               ...user,
+                               username: user.username,
+                               password: user.password,
+                               email: user.email,
+                               firstName: e.target.value,
+                               lastName: user.lastName
                            })}
                            placeholder={"first name"} className={`form-control mt-1`}/>
-                    <input value={this.state.lastName}
-                           onChange={(e) => this.setState({
+                    <input value={user.lastName}
+                           onChange={(e) => setUser({
+                               ...user,
+                               username: user.username,
+                               password: user.password,
+                               email: user.email,
+                               firstName: user.firstName,
                                lastName: e.target.value
                            })}
                            placeholder={"last name"} className={`form-control mt-1`}/>
-                    <button onClick={() => this.edit(this.state)}
+                    <button onClick={() => edit(user)}
                             className={`btn btn-success`}>Update Profile
                     </button>
 
@@ -61,3 +85,4 @@ export default class ProfileScreen extends React.Component {
     }
 };
 
+export default ProfileScreen;
