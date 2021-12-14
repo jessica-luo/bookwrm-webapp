a963aab16d38a1d383079662a2fbc9cfa138b820
jessica-luo <luo.jes@northeastern.edu>
2021-12-14 01:07:58 -0500

    details button on book list items added

--- web-dev-fp/src/components/BookList/BookListItem.js
+++ web-dev-fp/src/components/BookList/BookListItem.js
@@ -1,17 +1,25 @@
 import {ListGroupItem} from "reactstrap";
 import React from "react";
+import DetailsScreen from "../DetailsScreen";
 
 const BookListItem = ({book}) => {
 
     return (
-        <ListGroupItem className="p-4">
+        <ListGroupItem className="p-4 list-group-item-action">
+            <a href={`/details/${book.isbn}`}>
+                <button
+                    onClick={() => DetailsScreen(book.isbn)}
+                    className="btn btn-warning float-end ms-2">
+                    Details
+                </button>
+            </a>
             <div className="p-2 text-center">
                 <img src={book.cover} width="150" height="200" alt=""/>
             </div>
             <p className="mb-3">
                 <b>{book.title}</b>
                 <br/> <i>by {book.author}</i>
-            <br/> <span className={"wd-book-pages"}>{book.number_of_pages} pages </span>
+                <br/> <span className={"wd-book-pages"}>{book.number_of_pages} pages </span>
             </p>
         </ListGroupItem>
     );
