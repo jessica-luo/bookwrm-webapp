78570e641ad25b8e8d0a6c1ac45a2b65883a2fca
jessica-luo <luo.jes@northeastern.edu>
2021-12-14 01:17:00 -0500

    isbns added for featured/trending books

--- web-dev-fp/src/components/HomeScreen/trendingbooks.js
+++ web-dev-fp/src/components/HomeScreen/trendingbooks.js
@@ -3,18 +3,21 @@ export default [
         title: 'The Song of Achilles',
         author: 'Madeline Miller',
         number_of_pages: '416',
-        cover: 'https://upload.wikimedia.org/wikipedia/en/1/1e/TheSongofAchilles.png'
+        cover: 'https://upload.wikimedia.org/wikipedia/en/1/1e/TheSongofAchilles.png',
+        isbn: '0062060619'
     },
     {
         title: 'They Both Die At The End',
         author: 'Adam Silvera',
         number_of_pages: '384',
-        cover: 'https://upload.wikimedia.org/wikipedia/en/b/ba/They_Both_Die_at_the_End.jpg'
+        cover: 'https://upload.wikimedia.org/wikipedia/en/b/ba/They_Both_Die_at_the_End.jpg',
+        isbn: '0062457799'
     },
     {
         title: 'The Seven Husbands of Evelyn Hugo',
         author: 'Taylor Jenkins Reid',
         number_of_pages: '400',
-        cover: 'https://upload.wikimedia.org/wikipedia/en/1/17/The_Seven_Husbands_of_Evelyn_Hugo.jpg'
+        cover: 'https://upload.wikimedia.org/wikipedia/en/1/17/The_Seven_Husbands_of_Evelyn_Hugo.jpg',
+        isbn: '9781982147662'
     }
 ]
